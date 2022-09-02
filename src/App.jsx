b35c09f628b4171d8a8df617ff995ff5f760c569
JeeFzPiires <jeferson.piires@gmail.com>
2022-09-02 18:02:58 -0300

    implementando a logica

--- src/App.jsx
+++ src/App.jsx
@@ -3,22 +3,29 @@ import reactLogo from './assets/react.svg'
 
 function App() {
   const [count, setCount] = useState(0)
+  const [codContainer, setCodContainer] = useState(0)
 
-  const Pilha1 = [1,2,3]
-  const Pilha2 = [1,2,3]
-  const Pilha3 = [1,2,3]
-  const Pilha4 = [1,2,3]
+  function handleAddPilha() {
+    Pilha1.push(codContainer)
+
+    console.log(Pilha1)
+  }
+
+  const Pilha1 = []
+  const Pilha2 = []
+  const Pilha3 = []
+  const Pilha4 = []
 
   return (
     <div className="bg-slate-900 w-screen h-screen">
       <div>
         <h1 className='text-white'>Adicione o código do container</h1>
-        <input type="text" />
-        <button>Adicionar</button>
+        <input type="text" placeholder='Insira o código' value={codContainer} onChange={(e) => setCodContainer(e.target.value)}/>
+        <button className='bg-red-700' onClick={handleAddPilha}>Adicionar</button>
       </div>
       <div className="bg-blue-500 flex flex-wrap">
         <div className='basis-1/2'>
-          <h1>{Pilha1}</h1>
+          <h1>{codContainer}</h1>
         </div>
         <div className='basis-1/2'>
           <h1>Pilha 2</h1>
