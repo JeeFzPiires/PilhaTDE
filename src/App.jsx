888a2c28f0767bc35e8171b6f340cde2f0f3656a
JeeFzPiires <jeferson.piires@gmail.com>
2022-09-08 16:23:09 -0300

    Adicionando arrays

--- src/App.jsx
+++ src/App.jsx
@@ -2,49 +2,66 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 
 function App() {
-  const [pilhas, setPilhas] = useState([]);
   const [description, setDescription] = useState('')
-  const Pilha1 = []
-  const Pilha2 = []
-  const Pilha3 = []
-  const Pilha4 = []
+  const id = Math.floor(100)
+  const [pilha1, setPilha1] = useState([])
+  const [pilha2, setPilha2] = useState([])
+  const [pilha3, setPilha3] = useState([])
+  const [pilha4, setPilha4] = useState([])
 
   function handleAddNewContainer() {
-    const maxLength = 5;
-  
-    if(description.length == 0){
-      alert("A descrição não pode ser vazia")
-    } else {
-      setPilhas(pilhas.push(description));
-      setDescription("")
-    }
-    if(pilhas != null){
-      Pilha1.push(pilhas)
+    const newPilha = {
+      cod: (id * Math.floor(Math.random() * id)),
+      descricao: description,
     }
 
-    console.log(Pilha1)
+    const newPilhas = [ ...pilha1, newPilha];
+
+    if (pilha1.length >= 0 && pilha1.length < 3) {
+      setPilha1(newPilhas);
+    } 
+    else if (pilha2.length >= 0 && pilha2.length < 3) {
+      const newPilhas2 = [ ...pilha2, newPilha]
+      setPilha2(newPilhas2);
+    } 
+    else if (pilha3.length >= 0 && pilha3.length < 3) {
+      const newPilhas3 = [...pilha3, newPilha]
+      setPilha3(newPilhas3);
+    } 
+    else if(pilha4.length >= 0 && pilha4.length < 3) {
+      const newPilhas4 = [...pilha4, newPilha]
+      setPilha4(newPilhas4)
+    } else {
+      return alert("Todas Pilhas estão cheias remova, para adicionar um novo container")
+    }
+    setDescription('');
+    console.log(pilha1)
   }
 
+  const onChange = e => {
+    setDescription(e.target.value)
+  }
+  
   
   return (
     <div className="bg-slate-900 w-screen h-screen">
       <div>
         <h1 className='text-white'>Adicione o código do container</h1>
-        <input type="text" placeholder='Insira a descrição do container' value={description} onChange={(e) => setDescription(e.target.value)}/>
+        <input type="text" placeholder='Insira a descrição do container' value={description} onChange={onChange}/>
         <button className='bg-red-700' onClick={handleAddNewContainer}>Adicionar</button>
       </div>
       <div className="bg-blue-500 flex flex-wrap">
         <div className='basis-1/2'>
-          <h1>{description}</h1>
+          <h1 id='pilha1'>{pilha1.cod}</h1>
         </div>
         <div className='basis-1/2'>
-          <h1>Pilha 2</h1>
+          <h1>{pilha2.descricao}</h1>
         </div>
         <div className='basis-1/2'>
-          <h1>Pilha 3</h1>
+          <h1>{pilha3.descricao}</h1>
         </div>
         <div className='basis-1/2'>
-          <h1>Pilha 4</h1>
+          <h1>{pilha4.descricao}</h1>
         </div>
       </div>
     </div>
